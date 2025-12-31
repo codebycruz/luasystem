e852caed2e85af68dfd2e6db0a9871c52d5c59ac
David Cruz <david@codebycruz.com>
2025-12-31 04:12:24 -0800

    Fix race condition

--- build.lua
+++ build.lua
@@ -1,7 +1,4 @@
 local outDir = os.getenv("LPM_OUTPUT_DIR")
 local parentDir = outDir:match("^(.*)/[^/]+$")
 
--- os.execute("rm -rf " .. outDir)
-os.execute("make")
-
-os.execute("cp './src/system.so' '" .. parentDir .. "/system.so'")
+os.execute("make && cp './src/system.so' '" .. parentDir .. "/system.so'")
